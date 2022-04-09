458a82a5e66ddfe86afb9300a46172e58fba9e26
Mahalingam S <senguttuvan.mahalingam@simdev.ch>
2022-04-09 20:35:44 +0400

    xml and json deserialize

--- HaleyExtensions/Extensions/ObjectSerialization.cs
+++ HaleyExtensions/Extensions/ObjectSerialization.cs
@@ -8,11 +8,18 @@ using System.Text.Json.Serialization;
 using System.Xml;
 using System.Xml.Linq;
 using System.Xml.Serialization;
+using System.Collections;
 
 namespace Haley.Utils
 {
     public static class ObjectSerialization
     {
+        private static JsonSerializerOptions commonOptions = new JsonSerializerOptions() 
+        { 
+            WriteIndented = true ,
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault,
+        };
+
         public static XElement ToXml(this object source)
         {
             Type _type = source.GetType();
@@ -36,18 +43,53 @@ namespace Haley.Utils
             return XElement.Parse(sw.ToString());
         }
 
-        public static string ToJson(this object source,List<JsonConverter> converters = null)
+        public static T XmlDeserialize<T>(this string input)
         {
-            var _jsonOptions = new JsonSerializerOptions() {WriteIndented = true};
-                _jsonOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault;
-                _jsonOptions.Converters.Add(new JsonStringEnumConverter());
-            if (converters!= null && converters?.Count> 0)
+            Type _type = typeof(T);
+            XmlSerializer serializer = new XmlSerializer(_type); //New serializer for the given type.
+            XmlSerializerNamespaces ns = new XmlSerializerNamespaces(new[] { XmlQualifiedName.Empty });
+            StringReader rdr = new StringReader(input);
+            return (T)serializer.Deserialize(rdr);
+        }
+
+        public static T JsonDeserialize<T>(this string input)
+        {
+            return JsonDeserializeInternal<T>(input, commonOptions);
+        }
+        public static T JsonDeserialize<T>(this string input, JsonSerializerOptions options)
+        {
+            return JsonDeserializeInternal<T>(input, options);
+        }
+
+        private static T JsonDeserializeInternal<T>(string input,JsonSerializerOptions option)
+        {
+            return (T)JsonSerializer.Deserialize(input, typeof(T), options: option);
+        }
+
+        public static string ToJson(this object source,JsonSerializerOptions options)
+        {
+            return ToJsonInternal(source, ref options, null);
+        }
+        private static string ToJsonInternal(object source, ref JsonSerializerOptions options, List<JsonConverter> converters = null)
+        {
+            if (!options.Converters.Contains(new JsonStringEnumConverter()))
+            {
+                try
+                {
+                    options.Converters.Add(new JsonStringEnumConverter());
+                }
+                catch (Exception)
+                {
+                }
+            }
+
+            if (converters != null && converters?.Count > 0)
             {
                 foreach (var item in converters)
                 {
                     try
                     {
-                        _jsonOptions.Converters.Add(item);
+                        options.Converters.Add(item);
                     }
                     catch (Exception)
                     {
@@ -55,9 +97,12 @@ namespace Haley.Utils
                     }
                 }
             }
-            return JsonSerializer.Serialize(source, source.GetType(), _jsonOptions);
+            return JsonSerializer.Serialize(source, source.GetType(), options);
         }
-
+        public static string ToJson(this object source,List<JsonConverter> converters = null)
+        {
+            return ToJsonInternal(source,ref commonOptions, converters);
+        }
         public static string BinarySerialize(this object input)
         {
             string result = null;
