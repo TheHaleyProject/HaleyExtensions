a55c49dec3cbefcff4516e263f23e907bc7d06c8
S Mahalingam <senguttuv.mahalingam@daep.ae>
2023-11-17 13:21:32 +0400

    prorgress

--- HaleyExtensions/Extensions/StringHelpers.cs
+++ HaleyExtensions/Extensions/StringHelpers.cs
@@ -4,75 +4,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.NetworkInformation;
+using System.Text.Json;
 using System.Text.RegularExpressions;
 
 namespace Haley.Utils
 {
     public static class StringHelpers
     {
-        public static string PadCenter(this string source, int length, char character = '\u0000')
-        {
-            int space_available = length - source.Length;
-            int pad_left = (space_available / 2) + source.Length; //Amount to pad left.
-            if (character == '\u0000')
-            {
-                return source.PadLeft(pad_left).PadRight(length);
-            }
-            else
-            {
-                return source.PadLeft(pad_left, character).PadRight(length, character);
-            }
-        }
-
-        public static bool IsBase64(this string input)
-        {
-            if (string.IsNullOrEmpty(input) || input.Length % 4 != 0
-               || input.Contains(" ") || input.Contains("\t") || input.Contains("\r") || input.Contains("\n"))
-                return false;
-            try
-            {
-                Convert.FromBase64String(input);
-                return true;
-            }
-            catch (Exception)
-            {
-                return false;
-            }
-        }
-
-        /// <summary>
-        /// When a base64 string is provided as input, it will replace the "/ + = " signs.
-        /// </summary>
-        /// <returns> if input is not base64, it will return same input. Else it will return sanitized value.</returns>
-        public static string SanitizeJWT(this string input) {
-            if (!input.IsBase64()) return input;
-            string result = input.Replace('+', '-').Replace('/', '_').Replace("=", "");
-            return result;
-        }
-
-        public static string DeSanitizeJWT(this string input) {
-            //this cannot be base 64
-            if (input.IsBase64()) return input;
-            string result = input.Trim().Replace('_', '/').Replace('-', '+');
-            switch(result.Length % 4) {
-                case 2: result += "=="; break; //add two equl signs. so we have 4 character.
-                case 3: result += "="; break; //add one equal sign. so we have 4 character.
-            }
-            return result;
-        }
-
-        public static string ToNumber(this string input)
-        {
-            string numbered_key = string.Empty;
-            foreach (var _char in input)
-            {
-                int index = (int)_char % 32;
-                numbered_key += index;
-            }
-            return numbered_key;
-        }
-        public static CompareStatus? CompareWith(this string source, string target)
-        {
+        public static CompareStatus? CompareWith(this string source, string target) {
             var _pattern = @"\d+";
 
             //If either source or target is null, return null. Donot compare.
@@ -93,17 +32,176 @@ namespace Haley.Utils
             var _source_padded = Regex.Replace(source, _pattern, m => m.Value.PadLeft(max, '0'));
             var _target_padded = Regex.Replace(target, _pattern, m => m.Value.PadLeft(max, '0'));
 
-            switch (_source_padded.CompareTo(_target_padded))
-            {
+            switch (_source_padded.CompareTo(_target_padded)) {
                 case 0:
                     return CompareStatus.Equal;
                 case 1:
                     return CompareStatus.Greater;
                 case -1:
                     return CompareStatus.Lesser;
-            }
+                }
 
             return null;
+            }
+
+        public static string DeSanitizeJWT(this string input) {
+            //this cannot be base 64
+            if (input.IsBase64()) return input;
+            string result = input.Trim().Replace('_', '/').Replace('-', '+');
+            switch (result.Length % 4) {
+                case 2: result += "=="; break; //add two equl signs. so we have 4 character.
+                case 3: result += "="; break; //add one equal sign. so we have 4 character.
+                }
+            return result;
+            }
+
+        public static bool IsBase64(this string input) {
+            if (string.IsNullOrEmpty(input) || input.Length % 4 != 0
+               || input.Contains(" ") || input.Contains("\t") || input.Contains("\r") || input.Contains("\n"))
+                return false;
+            try {
+                Convert.FromBase64String(input);
+                return true;
+                } catch (Exception) {
+                return false;
+                }
+            }
+
+        public static bool IsValidJson(this string json) {
+            try {
+                JsonDocument.Parse(json); return true;
+                } catch (Exception ex) {
+                return false;
+                }
+            }
+
+        public static string PadCenter(this string source, int length, char character = '\u0000') {
+            int space_available = length - source.Length;
+            int pad_left = (space_available / 2) + source.Length; //Amount to pad left.
+            if (character == '\u0000')
+            {
+                return source.PadLeft(pad_left).PadRight(length);
+            }
+            else
+            {
+                return source.PadLeft(pad_left, character).PadRight(length, character);
+            }
+        }
+        /// <summary>
+        /// When a base64 string is provided as input, it will replace the "/ + = " signs.
+        /// </summary>
+        /// <returns> if input is not base64, it will return same input. Else it will return sanitized value.</returns>
+        public static string SanitizeJWT(this string input) {
+            if (!input.IsBase64()) return input;
+            string result = input.Replace('+', '-').Replace('/', '_').Replace("=", "");
+            return result;
+        }
+        /// <summary>
+        /// Convert the Json to dictionary
+        /// </summary>
+        /// <param name="input">Input json file</param>
+        /// <param name="result">output converted result</param>
+        /// <param name="searchlevel">0 - Makes search all levels.</param>
+        /// <returns></returns>
+        public static bool ToDictionary(string jsonInput, out List<Dictionary<string, object>> result, int searchlevel = 1) {
+            if (searchlevel < 0) searchlevel = 0;
+            return ToDictionary(jsonInput, searchlevel, 1, out result); //Current search level is always 1.
+            }
+
+        public static string ToNumber(this string input) {
+            string numbered_key = string.Empty;
+            foreach (var _char in input)
+            {
+                int index = (int)_char % 32;
+                numbered_key += index;
+            }
+            return numbered_key;
+        }
+        //static bool IsJsonArray(this string json)
+        //{
+        //    try
+        //    {
+        //    } catch (Exception)
+        //    {
+
+        static void DeepConvertJson(Dictionary<string, object> dic) {
+            string[] localkeys = dic.Keys.ToArray(); ;
+
+
+            //Loop each item to check if value is json.
+            foreach (var key in localkeys) {
+                if (dic.ContainsKey(key) && dic[key] != null) {
+                    //Confirm if the value is a string first.
+                    try {
+                        var valueStr = dic[key].ToString();
+                        if (!(valueStr.StartsWith("{") || valueStr.StartsWith("["))) continue; //No need to change this dictionary value.
+                        if (valueStr.Trim().StartsWith("{") && valueStr.Trim().EndsWith("}")) {
+                            dic[key] = JsonSerializer.Deserialize<Dictionary<string, object>>(dic[key].ToString());
+                            } else if (valueStr.Trim().StartsWith("[") && valueStr.Trim().EndsWith("]")) {
+                            //dic[key] =
+                            }
+
+
+                        } catch (Exception ex) {
+                        //log it
+                        continue;
+                        }
+
+                    }
+                }
+            }
+
+        //    }
+        //}
+        /// <summary>
+        /// Convert the Json to dictionary
+        /// </summary>
+        /// <param name="input">Input json file</param>
+        /// <param name="result">output converted result</param>
+        /// <param name="searchlevel">0 - Makes search all levels.</param>
+        /// <returns></returns>
+        static bool ToDictionary(string jsonInput, int searchlevel,int currentlevel, out List<Dictionary<string, object>> result)
+        {
+            //Input could be json array or single json.
+            result = new List<Dictionary<string, object>>();
+            if (jsonInput == null) return false;
+            
+            try
+            {
+                var jsonStr = jsonInput.Trim();
+                if (string.IsNullOrWhiteSpace(jsonStr) || !jsonStr.IsValidJson()) return false;
+
+                if (jsonStr.StartsWith("[") && jsonStr.EndsWith("]"))
+                {
+                    //Even if it is a normal value, it will 
+                    //Array
+                    var jsonStrList = JsonSerializer.Deserialize<List<Dictionary<string, object>>>(jsonStr);
+                    if (jsonStrList != null && jsonStrList is List<Dictionary<string, object>> inputList)
+                    {
+                        foreach (var dicLocal in inputList)
+                        {
+                            DeepConvertJson(dicLocal, childConvertKeys, forallkeys);
+                        }
+                        result = inputList;
+                    }
+                } else if (jsonStr.StartsWith("{") && jsonStr.EndsWith("}"))
+                {
+                    //object
+                    var resultRawDic = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonStr);
+                    if (resultRawDic != null && resultRawDic is Dictionary<string, object> inputDic)
+                    {
+                        DeepConvertJson(inputDic, childConvertKeys, forallkeys);
+                        result.Add(inputDic);
+                    }
+                }
+
+                //SchemaProps = JsonObject.Parse(SchemaProps.ToString());
+            } catch (Exception ex)
+            {
+                //throw;
+                return false;
+            }
+            return true;
         }
     }
 }
---
+++ Test/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Haley.Utils;
+using System.Text.Json;
+
+
+var jsonArray1 = "[4,3,5,2,2]";
+jsonArray1.IsValidJson();
+
+try
+{
+    var another = jsonArray1.Trim().Substring(1, jsonArray1.Length - 2);
+    var answer = another.Split(',');
+    var answer1 = JsonSerializer.Deserialize<string[]>(jsonArray1);
+} catch (Exception ex)
+{
+}
+var jsonArray2 = "{\"mpId\": 1311, \"mpName\": \"md_name\", \"mpDataType\": \"t_string\", \"mpKind\": \"user_value\", \"displayName\": \"Name\", \"isPredefined\": \"1\", \"shId\": 0, \"displayMpId\": 0, \"dbName\": null, \"tagGroupId\": 0}";
+jsonArray2.IsValidJson();
+
+try
+{
+    var answer2 = JsonSerializer.Deserialize<string[]>(jsonArray2);
+} catch (Exception ex)
+{
+}
+
+
+var jsonArray3 = "[{\"spId\": 1922, \"shId\": 2, \"mpId\": 1311, \"mpName\": \"md_name\", \"mpDataType\": \"t_string\", \"mpKind\": \"user_value\", \"displayName\": \"Title\", \"isPredefined\": \"1\", \"isVisible\": \"1\", \"isOptional\": \"0\", \"isArray\": \"0\", \"order\": 0, \"description\": \"Name of the Contract\", \"graph\": null, \"tagGroup\": null},{\"spId\": 1923, \"shId\": 2, \"mpId\": 1332, \"mpName\": \"md_code\", \"mpDataType\": \"t_string\", \"mpKind\": \"user_value\", \"displayName\": \"Code\", \"isPredefined\": \"1\", \"isVisible\": \"1\", \"isOptional\": \"0\", \"isArray\": \"0\", \"order\": 0, \"description\": \"Contract code\", \"graph\": null, \"tagGroup\": null},{\"spId\": 1924, \"shId\": 2, \"mpId\": 1338, \"mpName\": \"md_contract_distribution\", \"mpDataType\": \"t_bool\", \"mpKind\": \"attribute\", \"displayName\": \"Has Distribution\", \"isPredefined\": \"1\", \"isVisible\": \"1\", \"isOptional\": \"0\", \"isArray\": \"0\", \"order\": 0, \"description\": \"Does this contract have user distribution associated with it in CDE\", \"graph\": null, \"tagGroup\": null}]";
+jsonArray3.IsValidJson();
+
+try
+{
+    var answer3 = JsonSerializer.Deserialize<string[]>(jsonArray3);
+} catch (Exception ex)
+{
+}
+
+var jsonArray4 = "[{\"spId\": 1922, \"shId\": 2, \"mpId\": 1311, \"mpName\": \"md_name\", \"mpDataType\": \"t_string\", \"mpKind\": \"user_value\", \"displayName\": \"Title\", \"isPredefined\": \"1\", \"isVisible\": \"1\", \"isOptional\": \"0\", \"isArray\": \"0\", \"order\": 0, \"description\": \"Name of the Contract\", \"graph\": null, \"tagGroup\": null},{\"spId\": 1923, \"shId\": 2, \"mpId\": 1332, \"mpName\": , \"mpDataType\": \"t_string\", \"mpKind\": \"user_value\", \"displayName\": \"Code\", \"isPredefined\": \"1\", \"isVisible\": \"1\", \"isOptional\": \"0\", \"isArray\": \"0\", \"order\": 0, \"description\": \"Contract code\", \"graph\": null, \"tagGroup\": null},{\"spId\": 1924, \"shId\": 2, \"mpId\": 1338, \"mpName\": \"md_contract_distribution\", \"mpDataType\": \"t_bool\", \"mpKind\": \"attribute\", \"displayName\": \"Has Distribution\", \"isPredefined\": \"1\", \"isVisible\": \"1\", \"isOptional\": \"0\", \"isArray\": \"0\", \"order\": 0, \"description\": \"Does this contract have user distribution associated with it in CDE\", \"graph\": null, \"tagGroup\": null}]";
+jsonArray4.IsValidJson();
+
+var jsonArray5 = "{\"mpId\": 1311, \"mpName\": \"md_name\", \"mpDataType\": \"t_string\", \"mpKind\": \"user_value\", \"displayName\": \"Name\", \"isPredefined\": \"1\", \"shId\": \"[4,6,3,3]\", \"displayMpId\": 0, \"dbName\": null, \"tagGroupId\": 0}";
+jsonArray5.IsValidJson();
+
+
+
