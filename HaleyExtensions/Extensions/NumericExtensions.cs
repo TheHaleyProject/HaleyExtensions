404ea5766d5a0077f8ccbcd36620a089ee6235df
Lingam <rmsmech@gmail.com>
2024-07-12 12:26:28 +0400

    some wip changes

---
+++ HaleyExtensions/Extensions/NumericExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Haley.Extensions {
+    public static class NumericExtensions {
+        static string[] units = new string[] {"KB","MB","GB","TB","PB","EB"};
+        static int sizeSeparator = 1024;
+        public static string ToFileSize(this long input) {
+            if (input < sizeSeparator) return input.ToString() + " B";
+            long inputW = input;
+            int depth = 0;
+            bool parsed = false;
+            while (depth < units.Length && !parsed) {
+                inputW = Math.Round(inputW/)
+            }
+        }
+    }
+}
