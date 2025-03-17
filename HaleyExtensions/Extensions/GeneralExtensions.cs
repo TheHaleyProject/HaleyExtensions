491c3bd3452d611f6a7554d1146bf80a12282561
Quetzal <rmsmech@outlook.com>
2025-03-18 01:18:14 +0400

    optimizing the actual module implementation

---
+++ HaleyExtensions/Extensions/GeneralExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Haley.Abstractions;
+using System;
+
+namespace Haley.Utils
+{
+    public static class GeneralExtensions {
+       public static void Throw(this IFeedback input) {
+            if (input.Status) return;
+            throw new ArgumentException($@"Fail: {input.Message}");
+        }
+    }
+}
